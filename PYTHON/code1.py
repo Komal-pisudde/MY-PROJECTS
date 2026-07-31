9f2239f9ccca5181ccc44e8142ee7624f2bb4edc
Komal-pisudde <kkpisudde007@gmail.com>
2026-07-31 19:58:43 +0530

    updated files

---
+++ PYTHON/code1.py
@@ -0,0 +1,36 @@
+# Output
+print("Hello World")
+print("My name is Komal")
+print("I am learning Python")
+
+# Variables
+name = "Komal"
+age = 20
+price = 99.99
+
+print(name)
+print(age)
+print(price)
+
+# Data Types
+print(type(name))
+print(type(age))
+print(type(price))
+
+# Input
+name = input("Enter your name: ")
+print("Hello " + name)
+
+age = int(input("Enter your age: "))
+print("Your age is:", age)
+
+price = float(input("Enter price: "))
+print("Price is:", price)
+
+# Comments
+# This is a single-line comment
+
+"""
+This is a
+multi-line comment
+"""
---
+++ PYTHON/code2.py
@@ -0,0 +1,18 @@
+# Variables
+first_name = "Tony"
+last_name = "Stark"
+age = 53
+height = 1.85
+
+# Print details
+print("First Name:", first_name)
+print("Last Name:", last_name)
+print("Age:", age)
+print("Height:", height)
+
+# Take superhero name as input
+superhero = input("Enter Tony's superhero name: ")
+
+# Print superhero name
+print("Tony's superhero name is:", superhero)
+
