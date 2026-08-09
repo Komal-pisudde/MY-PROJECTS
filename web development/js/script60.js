63033359e43513e69922ebe57df35953125a2b02
Komal-pisudde <kkpisudde007@gmail.com>
2026-08-09 22:11:16 +0530

    updated files

---
+++ web development/js/script60.js
@@ -0,0 +1,27 @@
+console.log("This is strings tutorial")
+let a = "KOMAL";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+// console.log(a[5]);
+
+console.log(a.length)
+
+let real_name = "komal"
+let friend = "shru"
+console.log("Her name is " + real_name + " and her friends name is " + friend)
+console.log(`Her name is ${real_name} and her friends name is ${friend}`)
+
+let b = "shivanSh"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase()) 
+console.log(b.length) 
+console.log(b.slice(1, 5)) 
+console.log(b.slice(1)) 
+
+console.log(b.replace("Sh", "77"))
+console.log(b.concat(a, "shreya", "riya", "Priya"))
+
+console.log(b)
