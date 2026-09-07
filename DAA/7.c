8f764b25f2355bec0d196737dd0e8578f50ad806
Komal-pisudde <kkpisudde007@gmail.com>
2026-09-07 16:08:37 +0530

    updated files

---
+++ DAA/7.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+#define V 4
+#define INF 9999
+
+void printSolution(int dist[V][V])
+{
+    int i, j;
+
+    printf("Shortest distance matrix:\n");
+
+    for (i = 0; i < V; i++)
+    {
+        for (j = 0; j < V; j++)
+        {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void floydWarshall(int graph[V][V])
+{
+    int dist[V][V];
+    int i, j, k;
+
+    // Copy graph into distance matrix
+    for (i = 0; i < V; i++)
+    {
+        for (j = 0; j < V; j++)
+        {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    // Floyd-Warshall algorithm
+    for (k = 0; k < V; k++)
+    {
+        for (i = 0; i < V; i++)
+        {
+            for (j = 0; j < V; j++)
+            {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    printSolution(dist);
+}
+
+int main()
+{
+    int graph[V][V] =
+    {
+        {0,   5,   INF, 10},
+        {INF, 0,   3,   INF},
+        {INF, INF, 0,   1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph);
+
+    return 0;
+}
